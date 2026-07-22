8a28f82bf3cc49149ea3dbe3c891709553ba2670
Man Thakkar <manthakkar2005@gmail.com>
2026-07-22 10:59:28 +0530

    Simplify CORS configuration to allow all origins

--- server/index.js
+++ server/index.js
@@ -17,8 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://mern-task-manager-app.netlify.app", "http://localhost:3000", "http://localhost:3001"],
-    methods: ["GET", "POST", "DELETE", "PUT"],
+    origin: true,
     credentials: true,
   })
 );
